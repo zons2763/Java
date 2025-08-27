c54a49e0f396f315be9cf3bbf7d8d477ddf2f8c1
zons2763 <kdeld1026@gmail.com>
2025-08-27 15:16:20 +0900

    Commit

---
+++ chap03/src/sec02/exam02/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,38 @@
+package sec02.exam02;
+
+public class IncreaseDecreaseOperatorExample {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		int z;
+		
+		System.out.println("-----------------------");
+		x++;
+		++x;
+		System.out.println("x = " + x); // x = 12
+		
+		System.out.println("-----------------------");
+		y--;
+		--y;
+		System.out.println("y = " + y); // y = 8
+		
+		System.out.println("-----------------------");
+		z = x++;
+		System.out.println("z = " + z); // z = 12
+		System.out.println("x = " + x); // x = 13
+		
+		System.out.println("-----------------------");
+		z = ++x;
+		System.out.println("z = " + z); // z = 14
+		System.out.println("x = " + x); // x = 14
+		
+		System.out.println("-----------------------");
+		z = ++x + y++;
+		System.out.println("z = " + z); // z = 23
+		System.out.println("x = " + x); // x = 15
+		System.out.println("y = " + y); // y = 9
+
+	}
+
+}
---
+++ chap03/src/sec02/exam05/CharOperatorExample.java
@@ -0,0 +1,15 @@
+package sec02.exam05;
+
+public class CharOperatorExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A' + 1;
+		char c2 = 'A';
+		// char c3 = c2 = 1; // 산술 연산을 하면 int 타입이 된다.
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		// System.out.println("c3: " + c3);
+
+	}
+
+}
